ed80a6fa12898075ace6e4440fe36b9fa9685268
ascezis <writerzzz@mail.ru>
2023-10-04 18:04:09 +0700

    Learning script

---
+++ Calculator by ascezis/scripts.js
@@ -0,0 +1,27 @@
+let operation;
+function func() {
+    let result;
+
+    let num1 = document.getElementById('input_1').value;
+    let num_2 = document.getElementById('input_2').value;
+
+    switch (operation) {
+        case '+':
+            result = +(num1) + +(num_2);
+            break;
+        case '-':
+            result = num_2 - num1;
+            break;
+        case '/':
+            result = num_2 / num1;
+            break;
+        case '*':
+            result = num1 * num_2;
+            break;
+    }
+    document.getElementById('resulted').innerHTML = result;
+}
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', func);
